dfe17652d310d8a76a7d32c961e03a4245f5a978
vincent <vriccio512@gmail.com>
2019-08-18 16:39:14 +0000

    Action Version: 3.0

--- Actions/Tag_VM.py
+++ Actions/Tag_VM.py
@@ -1,7 +1,4 @@
-#use elif statement to define the tag, if vmName has some text then use a certain tag 
-#make sure to add the vmName_VM to the resourceNames section
-
-import requests
+#import requests
 import json
    
 def handler(context, inputs):
@@ -15,22 +12,18 @@ def handler(context, inputs):
        tag = 'Application Tier'
    elif 'DB' in vmName:
        tag = 'DB Tier'
-   elif 'Dev' in vmName:
-       tag = "Dev Machine"
-   elif 'Apache' in vmName:
-       tag = "apache machine"
      
    tags[tag] = 'tango-machine'
       
-   print('Tag VM action! tag: "{}".'.format(tag))
-   
-   slackMsg = ':label: Tag VM action! tag: "' + tag + '".'
-   body = {
-   "channel": "#general",
-   "username": "ABX",
-   "text": slackMsg,
-   "icon_emoji": ":bell:"
-   }
-   requests.post('https://hooks.slack.com/services/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', data=json.dumps(body), verify=False)
-       
+   #print('Tag VM action! tag: "{}".'.format(tag))
+      
+   #slackMsg = ':label: Is this mine? tag: "' + tag + '".'
+   #body = {
+   #"channel": "#general",
+   #"username": "ABX",
+   #"text": slackMsg,
+   #"icon_emoji": ":bell:"
+   #}
+   #requests.post('https://hooks.slack.com/services/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', data=json.dumps(body), verify=False)
+ 
    return { 'tags' : tags }
