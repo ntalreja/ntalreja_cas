5b19ecd57196592aa4ccb50ddf81a194d8f4285c
vincent <vriccio512@gmail.com>
2020-07-13 17:14:09 +0000

    Add new file

---
+++ vince_mc/getbpid.py
@@ -0,0 +1,8 @@
+def get_blueprints():
+  api_url = '{0}blueprint/api/blueprints'.format(api_url_base)
+  response= requests.get(api_url, headers=headers1, verify=False)
+  data = json.loads(response.content)
+  for i in data['content']:
+      print("Name" + i['name'])
+      print("ID" + i['id'])
+get_blueprints()
