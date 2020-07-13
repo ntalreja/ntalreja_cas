bf1acf67f6d09edccee3ce0171cc611600390369
vincent <vriccio512@gmail.com>
2020-07-13 17:14:56 +0000

    Update getbpid.py

--- vince_mc/getbpid.py
+++ vince_mc/getbpid.py
@@ -3,6 +3,6 @@ def get_blueprints():
   response= requests.get(api_url, headers=headers1, verify=False)
   data = json.loads(response.content)
   for i in data['content']:
-      print("Name" + i['name'])
-      print("ID" + i['id'])
+      print("Name " + i['name'])
+      print("ID " + i['id'])
 get_blueprints()
